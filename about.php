49750464d0aafa2dcb9590a08813f698ead1ed5f
NoahSekreter <nsekreter@gmail.com>
2018-01-03 12:55:26 -0500

    Fixing src refspec master issue

---
+++ about.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noah
+ * Date: 1/1/2018
+ * Time: 5:38 PM
+ */
+?>
+
+<html>
+    <head>
+        <!--Bootstrap-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+        <link href="style.css" rel="stylesheet">
+        <script src="script.js"></script>
+    </head>
+    <body class="TL-Body">
+        <div class="Nav-Container">
+            <div class="Logo-Container"><img src="Images/CH_Logo_Large.png"></div>
+            <div class="Nav-List">
+                <div class="Nav-Item"><a href="">PRODUCTS</a></div>
+                <div class="Nav-Item"><a href="">MARKETS</a></div>
+                <div class="Nav-Item"><a href="">ABOUT US</a></div>
+                <div class="Nav-Item"><a href="">ENGINEERED SOLUTIONS</a></div>
+                <div class="Nav-Item"><a href="">CONTACT US</a></div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <a href="">E-Mail Us</a>
+                </div>
+                <div class="col-md-6">
+                    <a href="">800-321-1874</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="TL-Container">
+            <div class="Banner">
+                <div class="Banner-Text">
+                    Innovative products and custom hardware development since 1916
+                </div>
+            </div>
+            <div class="TL-Highlight">
+                Cleveland Hardware is committed to produce and deliver defect-free products and services in a timely and cost-competitive manner in support of customer requirements.
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    1881, Cleveland Hardware Company founded from an iron works and wagon hardware firm by Leander McBride.
+                </div>
+                <div class="col-md-6">
+                    <img src="Images/Test-Image.jpg">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6"></div>
+                <div class="col-md-6">
+                    1900, Cleveland Hardware grew to over 750-1000 employees.
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    1906, a new plant was opened on 79th Street, where the current location of the plant stands today.
+                </div>
+                <div class="col-md-6"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="Images/Test-Image.jpg">
+                </div>
+                <div class="col-md-6">
+                    1909, Charles E. Adams became head of the firm. This is also when Auto parts began turning into a significant portion of the company’s line of products.
+                </div>
+            </div>
+            <div class="TL-Highlight">
+                Cleveland Hardware is customer focused and is continually striving to optimize its processes and products to better serve the needs of our customers.
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    1932, the Board of Directors changed the name to its current name of Cleveland Hardware & Forging Co.
+                </div>
+                <div class="col-md-6">
+                    <img src="Images/Test-Image.jpg">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6"></div>
+                <div class="col-md-6">
+                    1985, Cleveland Hardware was acquired by Green Bay Drop Forge.
+                </div>
+            </div>
+            <div class="TL-Highlight">
+                Today Cleveland Hardware continues some of the great traditions established throughout its long history. Adopting a culture of Lean Manufacturing, Cleveland Hardware continues to be a major producer and innovator of hardware for heavy commercial trucks, commercial and school buses, off-road vehicles, service vehicles, emergency vehicles, and private aircraft.
+            </div>
+        </div>
+    </body>
+</html>
---
+++ script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Noah on 1/3/2018.
+ */
